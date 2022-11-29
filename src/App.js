16f7d8f403d7f620cb6b4fa7a268b9b739c867a2
Stanislav Valkov <63970354+stanislavyv@users.noreply.github.com>
2022-11-29 19:33:26 +0200

    Add add-task button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import MainContainer from './components/main-container';
 import AppHeaderBar from './components/app-header-bar';
 import TasksList from './components/tasks-list';
+import AddButton from './components/add-button';
 
 function App() {
     return (
@@ -8,6 +9,7 @@ function App() {
             <MainContainer>
                 <AppHeaderBar />
                 <TasksList />
+                <AddButton />
             </MainContainer>
         </>
     );
---
+++ src/components/add-button/index.js
@@ -0,0 +1,25 @@
+import Fab from '@mui/material/Fab';
+import styled from '@mui/material/styles/styled';
+
+import AddIcon from '@mui/icons-material/Add';
+
+const StyledFab = styled(Fab)({
+    position: 'fixed',
+    bottom: '2%',
+    left: '2%',
+    backgroundColor: '#1976d2d1',
+
+    '&:hover': {
+        backgroundColor: '#005cb7d1',
+    },
+});
+
+const AddButton = () => {
+    return (
+        <StyledFab color="primary" aria-label="add">
+            <AddIcon />
+        </StyledFab>
+    );
+};
+
+export default AddButton;
